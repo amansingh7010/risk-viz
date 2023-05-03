1cc110a6d9d52a7eb2c120c2ec176d31a7962a95
Aman Singh <amansinghs7010@gmail.com>
2023-05-03 15:07:29 -0300

    add tabs

--- src/app/components/RiskApp.js
+++ src/app/components/RiskApp.js
@@ -5,11 +5,16 @@ import axios from "axios"
 import dynamic from 'next/dynamic'
 
 const RiskMap = dynamic(() => import('./RiskMap'), {ssr: false})
+const RiskTable = dynamic(() => import('./RiskTable'), {ssr: false})
+
+const selectedTabClasses = "bg-gray-800 text-blue-500"
+const nonSelectedTabClasses = "text-gray-400 hover:bg-gray-800 hover:text-gray-300"
 
 const RiskApp = ({ minYear, maxYear }) => {
   const [loading, setLoading] = useState(false)
   const [decade, setDecade] = useState(minYear);
   const [filteredData, setFilteredData] = useState([])
+  const [currentTab, setCurrentTab] = useState("map")
 
   const startDecade = Math.floor(minYear / 10) * 10;
   const endDecade = Math.ceil(maxYear / 10) * 10;
@@ -49,14 +54,24 @@ const RiskApp = ({ minYear, maxYear }) => {
 
   return (
     <div className="flex flex-col w-full">
-      <div className="w-1/4">
+      <div className="w-1/4 p-2">
         <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a decade</label>
         <select onChange={(e) => {setDecade(Number(e.target.value))}} id="countries" className="w-1/4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           {renderDecades()}
         </select>
     </div>
+    <div className="w-full m-2">
+      <ul class="flex flex-wrap border-b border-gray-200 dark:border-gray-700">
+        <li class="mr-4" tabIndex={0}>
+            <button class={`inline-block rounded-t-lg py-4 px-4 text-sm font-medium text-center ${currentTab === "map" ? selectedTabClasses : nonSelectedTabClasses}`} onClick={() => setCurrentTab("map")}>Risk Map</button>
+        </li>
+        <li class="mr-4" tabIndex={1}>
+            <button class={`inline-block rounded-t-lg py-4 px-4 text-sm font-medium text-center ${currentTab === "table" ? selectedTabClasses : nonSelectedTabClasses}`} onClick={() => setCurrentTab("table")}>Risk Table</button>
+        </li>
+      </ul>
+    </div>
     <div className="py-4">
-      <RiskMap data={filteredData} />
+      {currentTab === "map" ? <RiskMap data={filteredData} /> : <RiskTable data={filteredData} />}
     </div>
   </div>
   );
