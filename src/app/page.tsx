0ba13615570b2edd5e60c71b93b72a6ebef03b5e
Aman Singh <amansinghs7010@gmail.com>
2023-05-04 10:58:10 -0300

    make me stop with the icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import RiskApp from './components/RiskApp'
 import { loadData, getMaxYear, getMinYear } from './services/data'
+import { ShieldExclamationIcon } from '@heroicons/react/24/solid'
 
 export default async function Home() {
 
@@ -9,7 +10,10 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-start p-20">
       <header>
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-white">RiskThinking UI/UX Work Sample</h1>
+          <div className="flex">
+            <ShieldExclamationIcon className="w-9 mr-4" />
+            <h1 className="text-3xl font-bold tracking-tight text-white">RiskThinking UI/UX Work Sample</h1>
+          </div>
         </div>
       </header>
       <div className="z-10 w-full max-w-12xl items-center justify-between lg:flex">
